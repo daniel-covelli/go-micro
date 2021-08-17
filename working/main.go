a05ed11b2ddb624867c85372f4569245e825aa62
Daniel Covelli <danielcovelli@berkeley.edu>
2021-08-16 20:17:20 -0700

    create hello handler

--- working/main.go
+++ working/main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"working/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-		fmt.Fprintf(rw, "Hello %s\n", d)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye world")
-	})
-
-	http.ListenAndServe("127.0.0.1:9090", nil)
+	http.ListenAndServe("127.0.0.1:9090", sm)
 }
