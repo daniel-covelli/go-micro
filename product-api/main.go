da5258d3c4518242e28c53cea0fd195b583b902e
Daniel Covelli <danielcovelli@berkeley.edu>
2021-09-27 08:46:22 -0700

    add cors

--- product-api/main.go
+++ product-api/main.go
@@ -9,7 +9,9 @@ import (
 	"time"
 
 	"github.com/daniel-covelli/learn-go/product-api/handlers"
+
 	"github.com/go-openapi/runtime/middleware"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -55,10 +57,13 @@ func main() {
 	// Endpoint exposing swagger.yaml
 	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
+
 	// Instantiate server at port 9090 and invoke router instance.
 	s := &http.Server{
 		Addr:         ":9090",
-		Handler:      sm,
+		Handler:      ch(sm),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
